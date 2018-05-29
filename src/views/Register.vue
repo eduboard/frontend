c1c85953787ba1ccb8024dd009631f5dd53e69fc
Levi <levi@mailbox.tu-berlin.d>
2018-05-29 21:37:55 +0200

    register view design

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,17 +1,71 @@
 <template>
-  <div class="about">
-    <h1>This is the login page</h1>
+  <div class="container">
+    <div class="row top-buffer">
+      <div class="col-md-4 offset-md-4 text-center">
+        <h1><strong>eduBoard</strong></h1>
+        <h3>Benutzerkonto anlegen</h3>
+      </div>
+    </div>
+    <div class="row top-buffer">
+      <div class="col-md-4 offset-md-4 text-left">
+        <b-form action="POST">
+          <div class="form-details">
+            <b-form-group id="usernameLabel"
+                          label="E-Mail-Adresse"
+                          label-for="usernameInput">
+              <b-form-input type="text"
+                            name="username"
+                            placeholder="user@example.com"
+                            id="usernameInput">
+              </b-form-input>
+            </b-form-group>
+            <b-form-group id="passwordLabel"
+                          label="Passwort"
+                          label-for="passwordInput">
+              <b-form-input type="password"
+                            name="password"
+                            id="passwordInput">
+              </b-form-input>
+            </b-form-group>
+          </div>
+          <b-button type="submit"
+                    class="form-btn btn-lg btn-block btn-dark"
+                    onsubmit="">
+            Registrieren
+          </b-button>
+        </b-form>
+      </div>
+    </div>
+    <div class="row top-buffer">
+      <div class="col-md-4 offset-md-4 text center">
+        <p>
+          Bereits registriert?
+          <strong>
+            <router-link to="/login" class="toLogin">Anmelden</router-link>
+          </strong>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
-import Auth from '../components/Auth.vue';
-
 export default {
   components: {
-    Auth,
   },
 };
 
 </script>
+
+<style>
+
+.top-buffer {
+  margin-top: 25px;
+}
+
+.toLogin {
+  color: inherit;
+}
+
+</style>
