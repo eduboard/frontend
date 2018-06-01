370fa47b62e19893b7b961aad1efd895a5dfcc48
Peter Wielander <peter@17grad.com>
2018-06-01 16:36:59 +0200

    Prettify Auth View

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -2,7 +2,7 @@
   <div class="container auth">
     <div class="row top-buffer">
       <div class="col-md-6 offset-md-3 text-center">
-        <h1>eduBoard</h1>
+        <h1 class="font-weight-bold">eduBoard</h1>
         <h2>{{subtitle}}</h2>
       </div>
     </div>
@@ -13,7 +13,7 @@
             <label class="form__text" for="emailInput">
               E-Mail-Adresse
             </label>
-            <input class="form-control form__input"
+            <input class="form-control"
                    type="email"
                    placeholder="mail@beispiel.de"
                    id="emailInput">
@@ -22,41 +22,27 @@
             <label class="form__text" for="passwordInput">
               Passwort
             </label>
-            <input class="form-control form__input"
+            <input class="form-control"
                    type="password"
                    placeholder="•••••••••"
                    id="passwordInput">
           </div>
-          <div class="form-group form__item" v-if="action === 'register'">
+          <div class="form-group form__item appear-later" v-if="action === 'register'">
             <label class="form__text" for="passwordRepeatInput">
               Passwort wiederholen
             </label>
-            <input class="form-control form__input"
+            <input class="form-control"
                    type="password"
                    placeholder="•••••••••"
                    id="passwordRepeatInput">
           </div>
-<!--
-          <div class="form__item">
-            <span class="form__text">E-Mail-Adresse</span>
-            <input class="form__input" type="email" placeholder="mail@example.com">
-          </div>
-          <div class="form__item">
-            <span class="form__text">Passwort</span>
-            <input class="form__input" type="password" placeholder="•••••••••">
-          </div>
-          <div class="form__item" v-if="action === 'register'">
-            <span class="form__text">Passwort wiederholen</span>
-            <input class="form__input" type="password" placeholder="•••••••••">
-          </div> -->
-
-          <b-button type="button" class="font-weight-bold form-btn btn-lg btn-block btn-dark"
+          <b-button type="button" class="font-weight-bold"
             @click="sendFormDataToServer()">{{button}}
           </b-button>
         </b-form>
       </div>
     </div>
-    <div class="row top-buffer">
+    <div class="row top-buffer bottom__bottom">
       <div class="col-md-6 offset-md-3 text center">
         <p class="bottom">
           {{bottom}}
@@ -135,41 +121,34 @@ export default {
     text-align: center;
     font-weight: 700 !important;
     text-decoration: none;
+    padding-top: 6%;
   }
 
   h1 {
     color: black;
     padding-top: 10%;
-    padding-bottom: 2%;
+    padding-bottom: 5%;
   }
 
-  form {
-    padding: 7% 0;
-    color: black;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
-
-    &__item {
-
-    }
-
-    &__text {
-
-    }
-
-    &__input {
-      height: 50%;
-    }
+  h2 {
+    padding-bottom: 6%;
   }
 
-  button {
+  input {
+    outline: none;
+    border-bottom: 1px solid black;
+  }
+
+  b-button {
     background-color: black;
     color: white;
     border: none;
-    padding: 1rem 5rem;
-    max-width: 200px;
+    width: 100%;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    margin-top: 5%;
+    cursor: pointer;
+    text-align: center;
   }
 
   .bottom {
@@ -178,10 +157,29 @@ export default {
       color: black;
       padding-left: 50px;
     }
+
+    &__bottom {
+      padding-top: 5%;
+    }
   }
 
   .top-buffer {
     margin-top: 25px;
   }
+
+
+  @keyframes slideIn {
+    0% {
+      transform: translateY(-100%);
+    }
+    100% {
+      transform: translateY(0);
+    }
+  }
+  .appear-later {
+    transition: 1s ease-out 0s 1 slideIn;
+    // position: relative;
+  }
+
 
 </style>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="landing">
     <div class="container">
-      <h1> This is the filler landing page.</h1>
+      <h1 class="font-weight-bold""> This is the filler landing page.</h1>
       <h2> The landing page of the firm implementing this would be visible here.</h2>
       <!-- eslint-disable-next-line -->
-      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum commodi dignissimos porro architecto nemo similique vel voluptate odio deserunt ducimus! Cum, odit, voluptatum. Eius alias, in aperiam itaque repellat, officiis odio eligendi quam maxime, fugiat provident possimus dolore quos amet at cum voluptatum quia molestias aliquam. Aliquid nobis, eaque odio obcaecati ipsum fuga laudantium, atque tenetur, voluptate, dolorem inventore. Eligendi nisi repellendus doloribus, saepe reiciendis accusantium aspernatur veniam, optio reprehenderit, voluptates magni tenetur! Aliquam eligendi ea libero, illum perspiciatis facere incidunt sunt deleniti ab laboriosam, delectus corporis. Natus nihil reprehenderit molestias illo cum tempora adipisci deleniti ipsa, voluptate architecto neque dignissimos nesciunt, perferendis eligendi tenetur pariatur dolor optio in quibusdam accusamus, nostrum alias debitis quas animi. Rem odit, quo praesentium nihil saepe temporibus et animi nam explicabo ipsum quos, sunt? Modi minima fuga quae blanditiis, cupiditate doloribus, quidem, aperiam repellat quia consequatur, sed sint nemo. Ducimus placeat corrupti laboriosam suscipit maxime, quis, deserunt, tenetur quos aliquid asperiores quia architecto similique temporibus, nemo. Odio voluptatibus beatae, illum veniam nam a repellat animi. Qui est corporis, accusamus ipsam quisquam nobis impedit explicabo porro itaque recusandae harum corrupti pariatur perspiciatis facilis numquam dolor, modi, ullam vitae soluta aperiam molestiae fugit veniam maxime quas.</p>
-      <router-link class="login" to="/login">Login</router-link>
+      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum commodi dignissimos porro architecto nemo similique vel voluptate odio deserunt ducimus! Cum, odit, voluptatum. Eius alias, in aperiam itaque repellat, officiis odio eligendi quam maxime, fugiat provident possimus dolore quos amet at cum voluptatum quia molestias aliquam. Aliquid nobis, eaque odio obcaecati ipsum fuga laudantium, atque tenetur, voluptate, dolorem inventore. Eligendi nisi repellendus doloribus, saepe reiciendis accusantium aspernatur veniam, optio reprehenderit, voluptates magni tenetur! Aliquam eligendi ea libero, illum perspiciatis facere incidunt sunt deleniti ab laboriosam, delectus corporis. Natus nihil reprehenderit molestias illo cum tempora adipisci deleniti ipsa, voluptate architecto neque dignissimos nesciunt, perferendis eligendi tenetur pariatur dolor optio in quibusdam accusamus, nostrum alias debitis quas animi. Rem odit, quo praesentium nihil saepe temporibus et animi nam explicabo ipsum quos, sunt? Modi minima fuga quae blanditiis, cupiditate doloribus, quidem, aperiam repellat quia consequatur, sed sint nemo. Ducimus placeat corrupti laboriosam suscipit maxime, quis, deserunt, tenetur quos aliquid asperiores quia architecto similique temporibus, nemo. Odio voluptatibus beatae, illum veniam nam a repellat animi. Qui est corporis, accusamus ipsam quisquam nobis impedit explicabo porro itaque recusandae harum corrupti pariatur perspiciatis facilis numquam dolor, modi, ullam vitae soluta aperiam molestiae fugit veniam maxime quas.
+      </p>
+      <router-link class="login font-weight-bold"" to="/login"> Login </router-link>
     </div>
   </div>
 </template>
@@ -17,8 +18,16 @@ export default {
 /* eslint-disable */
 </script>
 
-<style>
+<style scoped>
+
+  p {
+    padding-bottom: 2%;
+  }
+
+  .landing, h1, h2, p {
+    padding-top: 5%;
+  }
   .login {
-    font-size: 2rem;
+    font-size: 3rem;
   }
 </style>
