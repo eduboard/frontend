4ac3d060c5735b993eff3421365ee2c6aff7e2ba
Peter Wielander <peter@17grad.com>
2018-06-08 12:41:35 +0200

    Add API and Store

---
+++ src/controllers/Api.js
@@ -0,0 +1,36 @@
+export default {
+  /*
+   * Request wrapper. Takes a callback and passes JSON response to it.
+   * Catched non-JSON responses. Handling of success/failure is left to the callback
+   */
+  request(method, url, callback, body) {
+    const req = new XMLHttpRequest();
+    req.onreadystatechange = function onResponse() {
+      // If a JSON response
+      if (this.readyState === 4 && req.responseText[0] === '{') {
+        try {
+          callback(JSON.parse(req.responseText));
+        } catch (e) {
+          callback({
+            success: false,
+            status: this.status,
+            msg: req.responseText,
+            error: 'Could not parse JSON response'
+          });
+        }
+      // If not a JSON response
+      } else if (this.readyState === 4) {
+        callback({
+          success: false,
+          status: this.status,
+          msg: req.responseText,
+        });
+      }
+    };
+    req.open(method, url, true);
+    if (typeof body !== 'undefined') {
+      req.setRequestHeader('Content-Type', 'application/json');
+    }
+    req.send(body);
+  }
+};
