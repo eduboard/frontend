2a863e48b2483ab2e18bb3e4f7b1bbe951f82e4e
Anne Schmidt <schmidt.10@campus.tu-berlin.de>
2018-06-15 11:17:25 +0200

    Geändertes Profile.vue mit Fehlern drin

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,12 +1,45 @@
 <template>
-  <div>
-    <div class="container">
-      <h1 class="">This is the filler user page.</h1>
-      <h2> The landing page of the firm implementing this would be visible here.</h2>
-      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum commodi
-        dignissimos porro architecto nemo similique vel voluptate odio deserunt.
-      </p>
+  <div class="container ">
+    <div class="">
+      <h2>Hier kannst Du Deine Profileinstellungen ändern:</h2>
     </div>
+    <div id="PictureWrapper" class="container -narrow auth" style="align-items:center;">
+           <div class="controls__user__image" style="align-items:center; width:200px; height:200px; border-radius:50%;" v-on:click="show('Hier kannst du in Zukunft Dein Bild bearbeiten')">
+            {{user.name.slice(0,1) + user.surname.slice(0,1)}}
+           </div>
+    </div>
+    <div class="container -narrow auth"  > <!-- pw und email ändern -->
+    <form class="form" action="POST" v-on:submit.prevent="">
+      <div class="form__item">
+        <input class="form__input"
+               type="email"
+               placeholder="mail@beispiel.de"
+               id="emailInput"
+               v-model="email"
+               autofocus>
+        <label class="form__text" for="emailInput">E-Mail-Adresse</label>
+      </div>
+      <div class="form__item">
+        <input class="form__input"
+               type="password"
+               placeholder="•••••••••"
+               id="passwordInput"
+               v-model="password">
+        <label class="form__text" for="passwordInput">Passwort</label>
+      </div>
+      <div class="form__item appear-later" v-if="action === 'register'">
+        <input class="form__input"
+               type="password"
+               placeholder="•••••••••"
+               id="passwordRepeatInput">
+        <label class="form__text" for="passwordRepeatInput">Passwort wiederholen</label>
+      </div>
+      <button type="submit" class="form__button"
+        @click="evaluateAction()">Änderungen speichern</button>
+    </form>
+
+    </div>
+
   </div>
 </template>
 
@@ -15,7 +48,14 @@
 export default {
   components: {},
   name: 'profile',
+  methods: {
+    show(msg) {
+        this.$alert.show(msg);
+    }
+  }
 };
+
+
 </script>
 
 <style>
